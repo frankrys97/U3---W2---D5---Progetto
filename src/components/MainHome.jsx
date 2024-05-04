05fb71d5c4b7307a2df3d12ca5d6656c866c12e7
Francesco Cristiano <frankrys97@gmail.com>
2024-05-04 03:54:02 +0200

    adds Geolocation

--- src/components/MainHome.jsx
+++ src/components/MainHome.jsx
@@ -1,14 +1,66 @@
 import { useState } from "react";
-import { Button, Col, Container, Row, Form } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Container,
+  Row,
+  Form,
+  Alert,
+  Spinner,
+} from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 const MyMain = () => {
+  const myKey = "bf3d8ddb4e74c37efd82c130596a96e3";
+
   const [search, setSearch] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
     navigate(`/home/:${search}`);
+  };
+
+  const handleGeolocation = () => {
+    setIsLoading(true);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log("Position:", position);
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        console.log("Latitude:", latitude);
+        console.log("Longitude:", longitude);
+        fetchCityName(latitude, longitude);
+      });
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+      <Alert variant="danger">
+        Geolocation is not supported by this browser.
+      </Alert>;
+    }
+  };
+
+  const fetchCityName = (latitude, longitude) => {
+    fetch(
+      `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${myKey}`
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw Error(response.statusText);
+        }
+      })
+      .then((data) => {
+        const cityName = data[0].name;
+        console.log("City Name:", cityName);
+        navigate(`/home/:${cityName}`);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => setIsLoading(false));
   };
 
   return (
@@ -19,7 +71,7 @@ const MyMain = () => {
             <h1 className="display-2 text-white fw-bold">
               <span className="text-warning">Sky</span> on screen
             </h1>
-            <Form className="d-flex" onSubmit={handleSubmit}>
+            <Form className="d-flex gap-2" onSubmit={handleSubmit}>
               <Form.Control
                 type="search"
                 value={search}
@@ -34,6 +86,12 @@ const MyMain = () => {
               <Button variant="warning" type="submit">
                 <i className="bi bi-search"></i>
               </Button>
+              <Button className="btn-geo" onClick={handleGeolocation}>
+                <i className="bi bi-crosshair"></i>
+              </Button>
+              <span>
+                {isLoading && <Spinner animation="border" variant="warning" />}
+              </span>
             </Form>
           </div>
         </Col>
--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -1,5 +1,4 @@
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../assets/logo.png";
 import { NavLink } from "react-router-dom";
@@ -11,18 +10,6 @@ function MyNavBar() {
         <Navbar.Brand as={NavLink} to={"/"}>
           <img src={logo} width="150" height="75" alt="SkySense logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="ms-auto d-flex flex-row gap-3"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <NavLink to={"/"} className="nav-link">
-              Home
-            </NavLink>
-          </Nav>
-        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
