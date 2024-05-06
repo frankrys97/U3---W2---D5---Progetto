9e3783252845be339b882655ef785e65af9820c4
Francesco Cristiano <frankrys97@gmail.com>
2024-05-06 08:59:06 +0200

    adds hr to divide

--- src/components/MainCityPage.jsx
+++ src/components/MainCityPage.jsx
@@ -196,7 +196,7 @@ const MyMainCityPage = (props) => {
                           className="d-flex justify-content-between align-items-center"
                         >
                           <Col>
-                            <p className="fw-bold">
+                            <p className="fw-bold m-0">
                               {
                                 new Date(item.dt_txt)
                                   .toDateString()
@@ -224,6 +224,7 @@ const MyMainCityPage = (props) => {
                           <Col className="d-none d-md-block">
                             <p>{item.weather[0].description}</p>
                           </Col>
+                          <hr className="m-0" />
                         </Row>
                       ))}
                     </Container>
